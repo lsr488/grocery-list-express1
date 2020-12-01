0ff601648e98818f0b56734b617fb8cc1a0d1d04
unknown <lsr488@gmail.com>
2020-11-30 22:48:13 -0500

    Item names are editable and push to the server.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,20 +2,73 @@ console.log('hello from public/javascript/index.js');
 
 const list = document.getElementById('grocery-list');
 
+let isEditable = false;
+
 list.addEventListener('click', function(e) {
 	// console.log(e.target);
-	// console.log(e.target.dataset.id);
-
 
 	if(e.target.dataset.type.includes('edit')) {
 		console.log('edit button!');
-		console.log(e.target);
+		// console.log(e.target);
+
+		isEditable = true;
+		// console.log("should be true:", isEditable);
+		let editBtn = e.target;
+		let saveBtn = document.getElementById(`${e.target.dataset.id}-save`);
+
+		let deleteBtns = document.querySelectorAll(`[data-type=delete`);
+		deleteBtns.forEach(btn => {
+			btn.classList.add('no-show');
+		});
+
+		let liItem = editBtn.previousElementSibling;
+
+		liItem.setAttribute('contenteditable', 'true');
+
+
+		saveBtn.classList.remove('no-show');
+		saveBtn.addEventListener('click', function(e) {
+			console.log('save button clicked');
+			editBtn.previousElementSibling.setAttribute('contenteditable', 'false');
+
+			// submit the changed item name to the api
+			let data = {
+				id: liItem.id,
+				item: liItem.textContent,
+				state: false
+			}
+
+			fetch(`/api/items/${e.target.id}`, {
+				method: 'PUT',
+				body: JSON.stringify(data),
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8'
+				}
+			})
+				.then(response => {
+					if(!response.ok) {
+						throw new Error("Could not reach website.");
+					}
+					return response.json();
+				})
+				.then(json => { console.log("json response:", json) })
+				.catch(err => console.error(err));
+
+
+			deleteBtns.forEach(btn => {
+				btn.classList.remove('no-show');
+			});
+
+			saveBtn.classList.add('no-show');
+
+			isEditable = false;
+		});
 	}
 
 	// to isolate the "delete" button
 	if(e.target.dataset.type.includes('delete')) {
 		console.log("delete button");
-		console.log(e.target);
+		// console.log(e.target);
 
 		e.target.parentElement.style.display = "none";
 
@@ -41,9 +94,10 @@ list.addEventListener('click', function(e) {
 	}
 
 	// to isolate the item for strikethrough
-	if(e.target.className.includes('form-item')) {
-		console.log("form-item");
-		console.log(e.target);
+	if(e.target.dataset.type.includes('item') && isEditable == false) {
+		// console.log("should be false:", isEditable);
+		console.log("item name clicked");
+		// console.log(e.target);
 		e.target.classList.toggle('checked');
 	
 		if(e.target.className.includes('checked')) {
